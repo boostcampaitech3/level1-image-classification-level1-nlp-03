44156d98087931f39599050218fcb2c5d47137e9
sangryul <ksl970330@naver.com>
2022-02-28 15:04:51 +0000

    add background remove code and baseline code

---
+++ KSR/remove_background/remove_background_test.py
@@ -0,0 +1,47 @@
+from rembg.bg import remove
+import numpy as np
+import io
+from PIL import Image
+from PIL import ImageFile
+import os
+from pathlib import Path
+
+# input_path = './incorrect_mask.jpg'
+# output_path = './incorrect_mask_bgremoved.jpg'
+
+# Uncomment the following line if working with trucated image formats (ex. JPEG / JPG)
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+# f = np.fromfile(input_path)
+# result = remove(f)
+# img = Image.open(io.BytesIO(result)).convert("RGB")
+# img.save(output_path)
+
+
+
+print("test start")
+train_input_path = '/opt/ml/input/data_edit/eval/images'
+train_input_nobg_path = '/opt/ml/input/data_edit_nobg/eval/images'
+path = Path(train_input_nobg_path)
+path.mkdir(parents=True, exist_ok=True)
+
+#background폴더 생성
+os.makedirs(train_input_nobg_path, exist_ok=True)
+
+#폴더안에 들어있는 이미지
+for image in os.listdir(train_input_path):
+    if image.startswith("."):
+        continue
+    img_path = os.path.join(train_input_path,image)
+
+    #remote background
+    f = np.fromfile(img_path)
+    result = remove(f)
+    img = Image.open(io.BytesIO(result)).convert("RGB")
+    
+    #make output
+    output_img_path = os.path.join(train_input_nobg_path,image)
+    img.save(output_img_path)
+        
+
+print("test end")
