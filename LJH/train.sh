7e982bb7fe51ae8a330da11ae251465777bb9809
wogkr810 <46811558+wogkr810@users.noreply.github.com>
2022-03-09 19:39:16 +0900

    Add files via upload

---
+++ LJH/train.sh
@@ -0,0 +1,36 @@
+SM_CHANNEL_TRAIN=/opt/ml/input/data/train/images \
+SM_MODEL_DIR=/opt/ml/base_psy/model \
+python train.py \
+--epochs 1 \
+--lr 2e-05 \
+--batch_size 16 \
+--valid_batch_size 64 \
+--model SwinTransformerLarge384 \
+--optimizer AdamW \
+--lr_decay_step 4 \
+--val_ratio 0.2 \
+--log_interval 20 \
+--name swin-large-384
+
+
+# SM_CHANNEL_TRAIN=/opt/ml/input/data/train/images \
+# SM_MODEL_DIR=/opt/ml/result/train \
+# python train.py \
+# --epochs 30 \
+# --lr 2e-05 \
+# --batch_size 16 \
+# --valid_batch_size 64 \
+# --model SwinTransformerLarge384 \
+# --optimizer AdamW \
+# --lr_decay_step 4 \
+# --val_ratio 0.1 \
+# --log_interval 20 \
+# --name swin-large-384
+
+
+# SM_CHANNEL_EVAL=/opt/ml/input/data/eval \
+# SM_CHANNEL_MODEL=/opt/ml/me/result/train/SwinTransformer-large-38424 \
+# SM_OUTPUT_DATA_DIR=/opt/ml/me/result/eval \
+# python inference.py \
+# --batch_size 64 \
+# --model SwinTransformerLarge384
